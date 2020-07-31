38ca8f2a8b782977f0f1458245fffe73b5528be1
honzatomek <rpi3.tomek@protonmail.com>
2020-07-31 14:41:00 +0200

    Step 74 - allow scrolling past the right edge of the screen

--- src/kilo.c
+++ src/kilo.c
@@ -498,9 +498,8 @@ void editorMoveCursor(int key) {                                         // {{{2
             }
             break;
         case ARROW_RIGHT:
-            if (E.cx != E.screencols - 1) {
-                E.cx++;
-            }
+            /* allow user to scroll past the right edge of the screen */
+            E.cx++;
             break;
         case ARROW_LEFT:
             if (E.cx != 0) {
